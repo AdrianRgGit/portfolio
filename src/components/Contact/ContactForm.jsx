3f039b82eac01413e86738c34943f45d961211b5
Adrián Ramírez <adrianramirezgalera@gmail.com>
2025-07-05 13:00:54 +0200

    feat: enhance Contact section with improved layout and add CustomSubmitButton component

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,32 +1,38 @@
 import CustomInputText from "../ui/CustomInputText";
+import CustomSubmitButton from "../ui/CustomSubmitButton";
 import CustomTextArea from "../ui/CustomTextArea";
 
 export default function ContactForm() {
   return (
-    <form action="" class="grid grid-cols-2 gap-x-4 gap-y-6">
-      <CustomInputText
-        id="name"
-        label="Nombre"
-        name="name"
-        placeholder="Tu nombre"
-        value=""
-      />
-      <CustomInputText
-        id="name"
-        label="Nombre"
-        name="name"
-        placeholder="Tu nombre"
-        value=""
-      />
-      <CustomInputText
-        id="name"
-        label="Nombre"
-        name="name"
-        placeholder="Tu nombre"
-        value=""
-        className="col-span-2"
-      />
-      <CustomTextArea label="Mensaje" id="message" className="col-span-2" />
-    </form>
+    <article className="space-y-6">
+      <h3 class="text-2xl font-semibold">Envíame un mensaje</h3>
+      <form action="" class="grid grid-cols-2 gap-x-4 gap-y-6">
+        <CustomInputText
+          id="name"
+          label="Nombre"
+          name="name"
+          placeholder="Tu nombre"
+          value=""
+        />
+        <CustomInputText
+          id="name"
+          label="Nombre"
+          name="name"
+          placeholder="Tu nombre"
+          value=""
+        />
+        <CustomInputText
+          id="name"
+          label="Nombre"
+          name="name"
+          placeholder="Tu nombre"
+          value=""
+          className="col-span-2"
+        />
+        <CustomTextArea label="Mensaje" id="message" className="col-span-2" />
+
+        <CustomSubmitButton text="Enviar" />
+      </form>
+    </article>
   );
 }
---
+++ src/components/ui/CustomSubmitButton.jsx
@@ -0,0 +1,16 @@
+export default function CustomSubmitButton({
+  text,
+
+  class: className,
+  ...props
+}) {
+  return (
+    <button
+      type="submit"
+      class={`btn-hover-fill group border-theme-black flex cursor-pointer items-center gap-2 rounded-full border px-4 py-2 shadow transition-shadow duration-300 hover:shadow-[0_0_15px_rgba(176,255,111,0.6)] ${className}`}
+      {...props}
+    >
+      {text}
+    </button>
+  );
+}
